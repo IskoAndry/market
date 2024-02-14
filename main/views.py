868b1403723b6cf89ac9f6d5461fc6dda87b62e0
IskoAndry <isakov4852@mail.ru>
2024-02-14 16:53:19 +0300

    настройка главной страницы

--- main/views.py
+++ main/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': False,
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+        
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis magni recusandae maxime sit, maiores repudiandae praesentium! Rerum explicabo amet, autem nihil debitis nostrum eius unde libero? Blanditiis, eos? Inventore, facilis '
+        
+    }
+    return render(request, 'main/about.html', context)
